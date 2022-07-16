bc9112a11c59ea5f27a68db578d96d0676b24153
AntonYakavenka <doomanton@gmail.com>
2022-07-16 22:13:24 +0300

    Samurai's way 84 - useState hook

--- src/components/Profile/ProfileHeader/ProfileHeader.tsx
+++ src/components/Profile/ProfileHeader/ProfileHeader.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import s from './ProfileHeader.module.css'
 import {Avatar, Paper} from '@material-ui/core'
-import ProfileStatus from './ProfileStatus/ProfileStatus'
+// import ProfileStatus from './ProfileStatus/ProfileStatus'
 import {ProfilePropsType} from '../Profile'
+import {ProfileStatusWithHooks} from './ProfileStatus/ProfileStatusWithHooks'
 
 export const ProfileHeader: React.FC<ProfilePropsType> = ({profile, status, updateStatus}) => {
   return (
@@ -24,7 +25,8 @@ export const ProfileHeader: React.FC<ProfilePropsType> = ({profile, status, upda
             // status
             //   ? <ProfileStatus status={status} updateStatus={updateStatus}/>
             //   : <span style={{opacity: '0'}}>invisible span</span>
-            <ProfileStatus status={status} updateStatus={updateStatus}/>
+            // <ProfileStatus status={status} updateStatus={updateStatus}/>
+            <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
           }
         </h4>
       </div>
---
+++ src/components/Profile/ProfileHeader/ProfileStatus/ProfileStatusWithHooks.tsx
@@ -0,0 +1,43 @@
+import React, {ChangeEvent, useState} from 'react'
+
+type ProfileStatusPropsType = {
+  status: string | null
+  updateStatus: (status: string) => void
+}
+
+export const ProfileStatusWithHooks: React.FC<ProfileStatusPropsType> = (props) => {
+
+  const [editMode, setEditMode] = useState(false)
+  const [status, setStatus] = useState(props.status)
+
+  const activateEditMode = () => {
+    setEditMode(true)
+  }
+  const deactivateEditMode = () => {
+    setEditMode(false)
+    props.updateStatus(status as string)
+  }
+  const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setStatus(e.currentTarget.value)
+  }
+
+  return (
+    <div>
+      {
+        !editMode &&
+        <span onDoubleClick={activateEditMode}>
+                {props.status || <span>Change status...</span>}
+        </span>
+      }
+
+      {
+        editMode &&
+        <input autoFocus
+               onChange={onStatusChange}
+               value={status as string}
+               onBlur={deactivateEditMode}/>
+      }
+
+    </div>
+  )
+}
