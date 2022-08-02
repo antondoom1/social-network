cc8e2dbd43593978271567cd04bd1b8d949c006a
AntonYakavenka <doomanton@gmail.com>
2022-08-02 19:50:27 +0300

    Samurai's way 91 - react, redux devtools extensions

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,4 +1,4 @@
-import {combineReducers, createStore, applyMiddleware} from 'redux'
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux'
 import {profileReducer} from './profile-reducer'
 import {dialogsReducer} from './dialogs-reducer'
 import {sidebarReducer} from './sidebar-reducer'
@@ -20,7 +20,9 @@ let rootReducer = combineReducers({
   app: appReducer
 })
 
-export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 // @ts-ignore
 window.store = store
